3f864dac20fe0c65ee2d2226ecdf803307efada6
ibrahim babal <ibrahim@yourcasual.dev>
2026-05-02 00:35:05 +0300

    feat: migrate Next.js app to Cloudflare OpenNext

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import { NextConfig } from "next";
+import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {};
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 export default withNextIntl(nextConfig);
+
+void initOpenNextCloudflareForDev();
